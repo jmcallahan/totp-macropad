86adb51f9389b8e8476f75a18a000edc8dc4b0d5
Jason Callahan [C] <jasoncallahan@rivian.com>
2026-07-27 13:03:34 -0500

    Updated code.py with current WIP.

---
+++ code.py
@@ -0,0 +1,23 @@
+import time
+import rtc
+import usb_cdc
+from adafruit_macropad import MacroPad
+from secrets import secrets
+
+macropad = MacroPad()
+r = rtc.RTC()
+macropad.pixels.brightness = 0.3
+
+STATE_LOCKED = 0
+STATE_WAIT_SYNC = 1
+STATE_UNLOCKED = 2
+
+current_state = STATE_LOCKED
+
+def sync_time_from_serial():
+    if usb_cdc.console.in_waiting > 0:
+        # Process the incoming time data
+        int(usb_cdc.console.readline().decode('utf-8'))
+    line = usb
+time.localtime(epoch)
+r.datetime = time.localtime(epoch)
